367b5d1933026a416db77932d4e47c944fef4890
unknown <you@example.com>
2024-06-20 11:34:37 +0800

    update ke-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:prj_uas_d1_24/Address/add_your_address.dart';
-import 'package:prj_uas_d1_24/Complete_Order/complete_order_app.dart';
+// import 'package:prj_uas_d1_24/Address/add_your_address.dart';
+// import 'package:prj_uas_d1_24/Complete_Order/complete_order_app.dart';
 import 'package:prj_uas_d1_24/payment/payment.dart';
 // import 'package:prj_uas_d1_24/Address/add_your_address.dart';
 import 'package:prj_uas_d1_24/signup_file/SignUp.dart';
